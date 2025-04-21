f8888e544b33abd692d9bdc8ce96a7ae4bf74624
HaoZhang <dexter199057@gmail.com>
2025-04-21 22:56:14 +0300

    init

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Basic route for testing
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+}); 
