1926b06b0cbcd9aab2c37c368bc3c050a991d630
Dinh Khanh Chi Nguyen <khanhchi.fe@gmail.com>
2025-05-02 22:36:37 +0300

    Update Post.js
    Add author validation and edit slug generation

--- models/Post.js
+++ models/Post.js
@@ -15,16 +15,43 @@ const postSchema = new mongoose.Schema({
 });
 
 // Generate a slug from the title before saving
-postSchema.pre('save', function(next) {
-    if (this.isModified('title') || this.isNew) {
-        this.slug = slugify(this.title, { lower: true, strict: true });
+postSchema.pre('save', async function(next) {
+    try {
+        console.log('Generating slug for title:', this.title);
+        if (this.isModified('title') || this.isNew) {
+            this.slug = slugify(this.title, { lower: true, strict: true });
+            console.log('Generated slug:', this.slug);
+        }
+        next();
+    } catch (err) {
+        console.error('Error generating slug:', err);
+        next(err);
+    }
+});
+
+// Validate the author
+postSchema.pre('save', async function(next) {
+    if (this.isNew && this.author) {
+        try {
+            console.log('Validating author ID:', this.author);
+            const author = await User.findById(this.author);
+            if (!author) {
+                const err = new Error('Author not found for ID:', this.author);
+                err.status = 400;
+                return next(err);
+            }
+            console.log('Author validated:', author);
+        } catch (err) {
+            return next(err);
+        }
     }
     next();
-});
+}
+);
 
 // Update the updatedAt field before saving
 postSchema.pre('save', function(next) {
-    this.updatedAt = new Date.now();
+    this.updatedAt = new Date();
     next();
 });
 
