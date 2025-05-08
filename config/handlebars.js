b528d1aec9cc73238db18ece8d0a21ea80d5a7d7
HaoZhang <dexter199057@gmail.com>
2025-05-09 00:43:19 +0300

    remove test code

--- config/handlebars.js
+++ config/handlebars.js
@@ -22,42 +22,11 @@ const configureHandlebars = (app) => {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
         return new Date(date).toLocaleDateString('en-US', options);
       },
-      // Range helper (used for pagination)
-      range: function(start, end) {
-        let result = [];
-        for (let i = start; i <= end; i++) {
-          result.push(i);
-        }
-        return result;
-      },
-      // Add helpers for math operations
-      add: function(a, b) {
-        return a + b;
-      },
-      multiply: function(a, b) {
-        return a * b;
-      },
       // Truncate text helper
       truncate: function(str, len) {
         if (!str) return '';
         if (str.length <= len) return str;
         return str.substring(0, len) + '...';
-      },
-      // Safe property lookup helper
-      lookup: function(obj, prop) {
-        if (!obj) return null;
-        // Use Object.prototype.hasOwnProperty for security
-        if (Object.prototype.hasOwnProperty.call(obj, prop)) {
-          return obj[prop];
-        }
-        return null;
-      },
-      // Safe assign helper for setting variables in templates
-      assign: function(varName, varValue, options) {
-        if (!options.data.root) {
-          options.data.root = {};
-        }
-        options.data.root[varName] = varValue;
       }
     }
   }));
